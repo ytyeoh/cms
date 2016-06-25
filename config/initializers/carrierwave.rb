bb1f4bcc9b0adb24808eee259f9fbf42e016ad7a
ytyeoh <yytcsl1526>
2016-06-25 14:38:18 +0800

    upload photo to s3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,16 +2,16 @@ CarrierWave.configure do |config|
   config.storage = :file
 
   # Use S3 if you want
-  # config.storage = :fog
-  # config.fog_credentials = {
-  #   provider:               'AWS',
-  #   region:                 '',
-  #   aws_access_key_id:      '',
-  #   aws_secret_access_key:  ''
-  # }
-  # config.fog_directory  = ''
-  # config.fog_public     = true
-  # config.fog_attributes = { 'Cache-Control' => 'max-age=315576000' }
+  config.storage = :fog
+  config.fog_credentials = {
+    provider:               'AWS',
+    region:                 ENV.fetch('AWS_REGION'),
+    aws_access_key_id:      ENV.fetch('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key:  ENV.fetch('AWS_SECRET_ACCESS_KEY')
+  }
+  config.fog_directory  = ENV.fetch('S3_BUCKET_NAME')
+  config.fog_public     = true
+  config.fog_attributes = { 'Cache-Control' => 'max-age=315576000' }
 
   config.enable_processing = !Rails.env.test?
 end
